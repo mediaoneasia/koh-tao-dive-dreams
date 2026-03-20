2e6863f3b446bdcb5cb0eaad35b30bf22ab176b9
rocketasia <onemediaasia@duck.com>
2026-03-20 19:55:33 +0800

    Update booking_inquiries.js

--- pages/api/booking_inquiries.js
+++ pages/api/booking_inquiries.js
@@ -34,7 +34,8 @@ export default async function handler(req, res) {
   }
 
   if (req.method === 'PATCH') {
-    const id = req.query.id;
+    let id = req.query.id;
+    if (Array.isArray(id)) id = id[0];
     if (!id) {
       return res.status(400).json({ error: 'Missing booking id in URL' });
     }
