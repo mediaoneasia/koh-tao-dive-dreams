83e708d3409aa033febbe8ff37ffaa8b2a1ebb1a
rocketasia <onemediaasia@duck.com>
2026-03-23 20:11:22 +0800

    d

--- src/pages/dive-sites/japanese-gardens.tsx
+++ src/pages/dive-sites/japanese-gardens.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import DiveSiteDetail from '@/components/DiveSiteDeta';
+import DiveSiteDetail from '@/components/DiveSiteDetail';
 
 const JapaneseGardens = () => {
   return (
