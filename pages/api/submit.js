f959dc13b498a8fbdbcb26e90ac1609caea4491f
one-media-asia <onemediaasia@duck.com>
2026-04-08 07:28:08 +0800

    cors

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -11,7 +11,9 @@ const supabase = SUPABASE_URL && SERVICE_ROLE_KEY
   : null;
 
 export default async function handler(req, res) {
-  applyCors(res);
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.divinginasia.com');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
